6b70a004acfb1b0f64b1e8e224cca462b81d8dfb
Nicolas <nicolasm15@hotmail.com>
2019-08-26 11:13:24 -0300

    New

--- #Exemplos Teoricos/26.1_ListaOrdenada2.cpp
+++ #Exemplos Teoricos/26.1_ListaOrdenada2.cpp
@@ -7,12 +7,12 @@ int main(){
     float x[10],hold;
     int i=0;
 
-    while(i <= 9){
+    while(i < 10){
         cout << "Digite o elemento " << i+1 << ": ";
         cin >> x[i];
         i++;
     }
-    for(int i = 1; i <= 9;i++){
+    for(int i = 1; i < 10; i++){
         for(int j = i; j > 0; j--){
             if(x[j] < x[j-1]){
                 hold = x[j];
@@ -23,7 +23,7 @@ int main(){
     }
     i = 0;
     while (i<=9){
-        cout << i+1 << "º Elemento: "<< x[i] << endl;
+        cout << i+1 << "Elemento: "<< x[i] << endl;
         i++;
     }
 return 0;}
--- #Exercicios Praticos Aleatorios/ex33_ordenacao_vetores.cpp
+++ #Exercicios Praticos Aleatorios/ex33_ordenacao_vetores.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
+#define SIZE 12
 int main(){
-    int vet_in[4], hold;
-    for(int i=0; i<4; i++){
+    int vet_in[SIZE], hold;
+    for(int i = 0; i < SIZE; i++){
         cout <<"[" << i << "]: ";
         cin >> vet_in[i];
     }
     
-    for(int i = 0; i < 3; i++)
-        for(int j = 0; j < 3; j++){      // 5 3 4 1
-            if(vet_in[j] > vet_in[j+1]){  // 5 > 3 HOLD = 5, [0] = 3, [1] = HOLD(5).
+    for(int i = 0; i < SIZE-1; i++)
+        for(int j = 0; j < SIZE-1; j++){     
+            if(vet_in[j] < vet_in[j+1]){  
                 hold = vet_in[j];  
                 vet_in[j] = vet_in[j+1];
                 vet_in[j+1] = hold;
             }
         }
 
-    for(int i=0; i<4; i++)
+    for(int i = 0; i < SIZE; i++)
         cout << vet_in[i] << endl;
     return 0;
 }
