921608c472a179937a37c6d4ef03cce220d16e0d
Nicolas <nicolasm15@hotmail.com>
2019-09-14 15:45:55 -0300

    New

---
+++ #AED/AED2/15_funcao_3.cpp
@@ -0,0 +1,12 @@
+/*
+Foi realizada pesquisa de algumas habitantes de certa região. De cada habitante foram 
+coletados os seguintes dados: sexo,cor dos olhos (azuis, verdes ou castanhos),cor dos 
+cabelos (louros, pretos ou castanhos) e idade. Faça função que leía esses dados em um 
+vetor de registro. O vetor de regístro deve ser enviado por referência.
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
